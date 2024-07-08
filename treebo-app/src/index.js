a8e70003f3be7f14dc57ee8d27136575a1f26790
Lucas Gabriel Garcia <lucasgabrielg287@gmail.com>
2024-07-08 14:49:24 -0300

    T002: feat - Alteração do componente de navegação do app e adição dos módulos do react-router-dom e axios

--- treebo-app/src/index.js
+++ treebo-app/src/index.js
@@ -1,17 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import PagesList from './pages/List/List';
+import PagesAddTaks from './pages/AddTask/AddTask';
+import Home from './pages/Home/Home';
+
+const router = createBrowserRouter ([
+  {
+    path: "/",
+    element: <App/>,
+    children: [
+      {
+        path: "/",
+        element: <Home/>
+      },
+      {
+        path: "list",
+        element: <PagesList/>
+      },
+      {
+        path: "add-task",
+        element: <PagesAddTaks/>
+      }
+    ]
+  },
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
